8b0a896a7bc27edadf639da18ae1fee13236cdc8
Mathys27 <116517507+Megamaniac27@users.noreply.github.com>
2023-02-15 20:27:26 +0100

    Create sujet1.py

---
+++ sujet1.py
@@ -0,0 +1,15 @@
+'''
+Programmer la fonction verifie qui prend en paramètre un tableau de valeurs 
+numériques non vide et qui renvoie True si ce tableau est trié dans l’ordre croissant, 
+False sinon.
+'''
+
+def verifie(liste):
+    for i in range (1,len(liste)):
+        if liste[i]<liste[i-1]:
+            return False
+    return True
+
+            
+
+verifie([8, 12, 4])
